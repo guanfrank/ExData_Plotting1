9921b52fbcff363173ea76f25aff9d3d065c7f4f
Frank G <cmingkuan@gmail.com>
2016-09-18 02:56:57 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,8 @@
 library(sqldf)
 subEPC  <- read.csv.sql("household_power_consumption.txt", header=TRUE, sep=';', colClasses=c("Date"="character"), sql = "select * from file where Date == '1/2/2007' OR Date == '2/2/2007'")
-<<<<<<< HEAD
+
 png("plot1.png",width=480,height=480,units="px",bg="transparent")
-=======
-png("plot1.png",width=480,height=480)
->>>>>>> 4566463e7ef54bce874a0c6b7bc3c79c92ca0c6c
+
+
 hist(subEPC$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",col="red")
 dev.off()
